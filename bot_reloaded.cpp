f92b35609f5086086f0fa7c8aa8373e56b9ce09e
lucasberti <berti.lucas@gmail.com>
2016-03-16 20:07:04 -0300

    Adicionando quick chat do Rocket League.

--- bot_reloaded.cpp
+++ bot_reloaded.cpp
@@ -20,12 +20,25 @@ int main()
 		// Get current POSIX
 		std::time_t t = std::time(0);
 
+		boost::regex rl_simples("^([1-4])$");
+		boost::regex rl_composto("^([1-4])([1-4])$");
+		boost::match_results<std::string::const_iterator> regexResult;
+
 		// Prevents cross-interaction and old messages.
 		if (message->from->id != 82017295 && t - message->date < 10) 
 		{
 			/* ///// I could call onAnyMessage from each file, but it is easier to manage from here. ///// */
-			checkForAudio(message);
+
+			// Global checks
+			//checkForAudio(message);
 			checkForEstique(message);
+
+			if (boost::regex_match(message->text, regexResult, rl_composto, boost::match_default) || boost::regex_match(message->text, regexResult, rl_simples, boost::match_default))
+			{
+				std::cout << "Matching: " << regexResult[0] << std::endl;
+				checkQuickChat(message, regexResult[1], regexResult[2]);
+			}
+
 		}
 		else
 			std::cout << "Too old!" << std::endl;
@@ -33,6 +46,7 @@ int main()
 		printf("%s\n", message->text.c_str());
 
 	});
+
 	try
 	{
 		TgBot::TgLongPoll longPoll(bot);
@@ -44,7 +58,7 @@ int main()
 	}
 	catch (TgBot::TgException& e)
 	{
-		printf("%s\n", e.what());
+		printf("DEU POBREMA EM... %s\n", e.what());
 	}
 	return 0;
 }
