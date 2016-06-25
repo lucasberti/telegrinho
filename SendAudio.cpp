6c2ca8c92f6c9f662b5329256a7f81dd0cc4608f
lucasberti <berti.lucas@gmail.com>
2016-06-25 17:28:03 -0300

    Passando pra seleção composta

--- SendAudio.cpp
+++ SendAudio.cpp
@@ -12,39 +12,39 @@ void checkForAudio(TgBot::Message::Ptr message)
 		getBot().getApi().sendVoice(message->chat->id, MISS_NORDESTINA_FILE_ID);
 	}
 
-	if (message->text == "!nordestina" || message->text == "/nordestina")
+	else if (message->text == "!nordestina" || message->text == "/nordestina")
 	{
 		std::srand(time(0));
 		int r = rand() % 19;
 		getBot().getApi().sendVoice(message->chat->id, MISS_NORDESTINA_CHAT[r]);
 	}
 
-	if (message->text == "!dolar" || message->text == "/dolar")
+	else if (message->text == "!dolar" || message->text == "/dolar")
 	{
 		getBot().getApi().sendVoice(message->chat->id, MISS_NORDESTINA_DOLAR);
 	}
 
-	if (message->text == "churos" || message->text == "Churos" || message->text == "CHUROS")
+	else if (message->text == "churos" || message->text == "Churos" || message->text == "CHUROS")
 	{
 		getBot().getApi().sendVoice(message->chat->id, MISS_NORDESTINA_FILE_ID);
 	}
 
-	if (message->text == "xischupano")
+	else if (message->text == "xischupano")
 	{
 		getBot().getApi().sendVoice(message->chat->id, XIS_CHUPANO_FILE_ID);
 	}
 
-	if (message->text == "trevah")
+	else if (message->text == "trevah")
 	{
 		getBot().getApi().sendVoice(message->chat->id, TREVAH_FILE_ID);
 	}
 
-	if (message->text == "retchagemeno")
+	else if (message->text == "retchagemeno")
 	{
 		getBot().getApi().sendVoice(message->chat->id, RETCHA_GEMENDO_FILE_ID);
 	}
 	
-	if (message->text == "meu deus")
+	else if (message->text == "meu deus")
 	{
  		getBot().getApi().sendVoice(message->chat->id, HASTAD_MEU_DEUS);
 	}
